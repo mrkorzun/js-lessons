52386618f383d5a7875d8ab62069cdc85532691d
Mario Korzun <rkorzun2020@gmail.com>
2026-03-27 19:18:14 +0400

    Implement formatMessage function to truncate messages exceeding maxLength with ellipsis

--- js/HW-1/27.03/task-2.js
+++ js/HW-1/27.03/task-2.js
@@ -12,6 +12,14 @@
 // то функція обрізає рядок до maxLength символів,
 // додає трикрапку "..." в кінці та повертає обрізану версію.
 
+function formatMessage(message, maxLength) {
+  if (message.length <= maxLength) {
+    return message;
+  } else {
+    return message.slice(0, maxLength) + '...';
+  }
+}
+
 console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
 console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
 console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
