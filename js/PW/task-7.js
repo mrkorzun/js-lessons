12456f96f3c1ca6ce59fea9d4ec6def6e15811c9
Romario Korzun <60797864+mrkorzun@users.noreply.github.com>
2026-03-30 17:57:09 +0400

    Refactor index.html to comment out task-6.js and include task-7.js for new functionality

---
+++ js/PW/task-7.js
@@ -0,0 +1,29 @@
+// Створіть масив styles з елементами 'jazz' і 'blues'
+// Додайте до кінця масиву елемент 'rock-n-roll' за допомогою відповідного методу масивів
+// Знайдіть елемент 'blues' у масиві та замініть його на 'classic',
+
+// Напишіть функцію logItems(array), яка приймає масив як аргумент
+// і виводить у консоль кожен його елемент у форматі:
+// "<номер елемента> - <значення елемента>".
+// Використайте цикл for для перебору елементів масиву.
+// Нумерація елементів повинна починатися з 1 (а не з 0).
+
+// indexOf - only one item
+// length - all elements
+
+const styles = ['jazz', 'blues'];
+styles.push('rock-n-roll'); // add
+const findIndex = styles.indexOf('blues'); // find index
+
+if (findIndex !== -1) {
+  //   styles[findIndex] = 'classic';
+  styles.splice(findIndex, 1, 'classic');
+}
+
+function logItems(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`${i + 1} - ${array[i]}`);
+  }
+}
+
+logItems(styles);
