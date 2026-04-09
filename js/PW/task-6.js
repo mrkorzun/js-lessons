3d0f1f83904fc38548b36675c2ceddcb78d75513
Mario Korzun <rkorzun2020@gmail.com>
2026-04-09 17:41:18 +0400

    Refactor task files and add new tasks
    - Updated task-6.js with new tasks related to arrays and objects.
    - Created task-1.js with a simple addition function.
    - Created task-2.js with functions for screen size and BMI calculation.
    - Created task-3.js with various functions for string manipulation and user input.
    - Created task-4.js with functions for logical operations and string checks.
    - Created task-5.js with functions for loops and number comparisons.
    - Created task-6.0.js with initial tasks for file name extraction and order quantity.
    - Created task-1.js in SPW directory for future tasks.

--- js/PW/task-6.js
+++ js/PW/task-6.js
@@ -1,81 +1,282 @@
-// ==== Task 1 ====
+// ==== TASK 1 ====
 
-// Функція getFileName(file) приймає один параметр
+// Масив books містить колекцію об'єктів книг,
+// кожен з яких містить властивості title, author, rating.
+// Використовуючи метод map(), зроби так,
+// щоб у змінній titles вийшов масив назв усіх книг (властивість title) з масиву books.
 
-// file - рядок з іменем файлу.
-// Ім'я файлу може бути з розширенням, наприклад: styles.css, app.js або без, наприклад: styles, app.
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Tell-Tale Heart',
+//     author: 'Edgar Allan Poe',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
 
-// Використовуючи методи indexOf та slice, доповни код функції таким чином, щоб:
+// const titles = books.map(book => book.title);
 
-// Вона перевіряла наявність розширення в імені файлу (назва розширення відокремлюється від імені файла крапкою)
-// Якщо ім'я файлу не містить розширення, то функція повертала новий підрядок, що містить ім'я файлу без змін
-// В іншому разі функція повертала підрядок з іменем файлу, але без розширення
-/*
-function getFileName(file) {
-  // const index = file.indexOf('.');
-  //   if (index === -1) {
-  //     return file;
-  //   } else {
-  //     return file.slice(0, index);
-  //   }
+// console.log(titles); // ["The Last Kingdom", "Beside Still Waters", "The Tell-Tale Heart", "Redder Than Blood", "Enemy of God"]
 
-  // second variant
-  const index = file.indexOf('.');
-  return index === -1 ? file : file.slice(0, index);
-}
+// ==== TASK 2 ====
+// Масив books містить колекцію об'єктів книг,
+// кожен з яких містить властивість genres, значенням якої є масив жанрів.
+// Використовуючи метод flatMap(), зроби так,
+// щоб у змінній genres вийшов масив жанрів усіх книг (властивість genres) з масиву books.
 
-console.log(getFileName('styles.css')); // повертає "styles"
-console.log(getFileName('app.js')); // повертає "app"
-console.log(getFileName('app')); // повертає "app"
-console.log(getFileName('index.js')); // повертає "index"
-console.log(getFileName('index.html')); // повертає "index"
-console.log(getFileName('index.css')); // повертає "index"
-console.log(getFileName('index')); // повертає "index"
-*/
-// ==== Task 2 ====
-// Функція getOrderQuantity(order) приймає один параметр order
-// - масив рядків, які описують продукти в замовленні клієнта.
-// Доповни код функції таким чином, щоб вона повертала число, що дорівнює кількості елементів масиву.
-/*
-function getOrderQuantity(order) {
-  return order.length;
-}
-getOrderQuantity(['apple', 'peach', 'pear', 'banana']);
-getOrderQuantity(['apple', 'banana']);
-getOrderQuantity(['apple', 'banana', 'pear']);
-*/
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     genres: ['adventure', 'history'],
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     genres: ['fiction'],
+//   },
+//   {
+//     title: 'Redder Than Blood',
+//     author: 'Tanith Lee',
+//     genres: ['horror', 'mysticism'],
+//   },
+// ];
 
-// ==== Task 3 ====
-// Функція getLastElementMeta(array) приймає один параметр array - масив довільних значень.
-// Доповни код функції таким чином, щоб вона повертала новий масив з двох елементів:
+// const genres = books.flatMap(book => book.genres);
 
-// перший елемент - це індекс останнього елементу у масиві array
-// другий елемент - це значення останнього елементу у масиві array
-/*
-function getLastElementMeta(array) {
-  const lastIndex = array.length - 1; // сначала находим индекс масива
-  const lastElement = array[lastIndex]; // ищем элемент масыва
+// console.log(genres); // genres - це масив ["adventure", "history", "fiction", "horror", "mysticism"]
 
-  return [lastIndex, lastElement]; // выводим индекс и элемент масыва вместе
-}
+// ==== TASK 3 ====
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику функції із завдання.
 
-console.log(getLastElementMeta(['apple', 'peach', 'pear', 'banana']));
-console.log(getLastElementMeta(['apple', 'peach', 'pear']));
-console.log(getLastElementMeta(['apple', 'peach']));
-console.log(getLastElementMeta(['apple']));
-*/
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ['ipsum', 'lorem'],
+//     gender: 'male',
+//     age: 37,
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+//     gender: 'female',
+//     age: 34,
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+//     gender: 'male',
+//     age: 24,
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ['adipisicing', 'irure', 'velit'],
+//     gender: 'female',
+//     age: 21,
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ['ex', 'culpa', 'nostrud'],
+//     gender: 'male',
+//     age: 27,
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ['non', 'amet', 'ipsum'],
+//     gender: 'male',
+//     age: 38,
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ['lorem', 'veniam', 'culpa'],
+//     gender: 'female',
+//     age: 39,
+//   },
+// ];
 
-// Функція getExtremeElements(array) приймає один параметр array - масив елементів довільної довжини.
-// Доповни код функції таким чином, щоб вона
-// повертала масив з двох елементів - першого і останнього елементів параметра array.
-/*
-function getExtremeElements(array) {
-  // const lastIndex = array.length - 1;
+// const getUserEmails = users => users.map(user => user.email);
 
-  return [array[0], array[array.length - 1]];
-}
+// Доповни стрілочну функцію getUserEmails(users) таким чином,
+// щоб вона повертала масив поштових адрес користувачів (властивість email)
+// з масиву об'єктів в параметрі users.
 
-console.log(getExtremeElements([1, 2, 3, 4, 5])); // [1, 5]
-console.log(getExtremeElements(['Earth', 'Mars', 'Venus'])); //повертає ["Earth", "Venus"]
-console.log(getExtremeElements(['apple', 'peach', 'pear', 'banana'])); //повертає ["apple", "banana"]
-*/
+// console.log(getUserEmails(users)); // ["moorehensley@indexia.com", "sharlenebush@tubesys.com", "rossvazquez@xinware.com", "elmahead@omatom.com", "careybarr@nurali.com", "blackburndotson@furnigeer.com", "shereeanthony@kog.com"]
+
+// ==== TASK 4 ====
+// Доповни код так, щоб у змінній evenNumbers був масив парних чисел із масиву numbers,
+// а у змінній oddNumbers — масив непарних.
+// Обов'язково використовуй метод filter().
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
+// ==== TASK 5 ====
+// Масив books містить колекцію об'єктів книг,
+// кожен з яких містить властивості title, author, rating.
+// Використовуючи метод filter(), доповни код таким чином, щоб:
+
+// У змінній topRatedBooks утворився масив книг,
+// рейтинг яких (властивість rating) більший за або дорівнює значенню змінної MIN_RATING.
+
+// У змінній booksByAuthor утворився масив книг,
+// написаних автором з ім'ям (властивість author), яке збігається зі значенням у змінній AUTHOR.
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Tell-Tale Heart',
+//     author: 'Edgar Allan Poe',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = 'Bernard Cornwell';
+
+// const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
+// const booksByAuthor = books.filter(book => book.author === AUTHOR);
+
+// console.log(topRatedBooks); // 8
+// console.log(booksByAuthor); // "Bernard Cornwell"
+
+// ==== TASK 6 ====
+
+// Цей масив об'єктів ми будемо передавати в параметр users
+// під час виклику функції із завдання.
+
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+  },
+];
+
+// Доповни функцію getUserWithEmail(users, email) таким чином,
+// щоб вона повертала об'єкт користувача,
+// пошта якого (властивість email) збігається зі значенням другого параметра email.
+
+const getUserWithEmail = (users, email) =>
+  users.find(user => user.email === email);
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // повертає об'єкт користувача з ім'ям Sheree Anthony
