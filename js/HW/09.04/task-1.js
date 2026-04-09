ebef0b2b2f269e96686df23e9449a284df05df54
Mario Korzun <rkorzun2020@gmail.com>
2026-04-09 16:54:03 +0400

    Update index.html to uncomment task-5.js and comment out task-6.js

--- js/HW/09.04/task-1.js
+++ js/HW/09.04/task-1.js
@@ -4,6 +4,8 @@
 // яка прийматиме один параметр users — масив об’єктів користувачів.
 // Функція має повертати масив імен усіх користувачів (властивість name) із масиву users.
 
+const getUserNames = users => users.map(user => user.name);
+
 console.log(
   getUserNames([
     {
