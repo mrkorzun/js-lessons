fb239e02c09d345e1a31434d4d762cd3a0cef710
Romario Korzun <60797864+mrkorzun@users.noreply.github.com>
2026-04-09 13:55:58 +0400

    Add task-5.js to filter top-rated books and books by author

--- js/PW-01/task-6.js
+++ js/PW-01/task-6.js
@@ -160,3 +160,43 @@
 
 // console.log(evenNumbers);
 // console.log(oddNumbers);
+
+// ==== TASK 5 ====
+// Масив books містить колекцію об'єктів книг,
+// кожен з яких містить властивості title, author, rating.
+// Використовуючи метод filter(), доповни код таким чином, щоб:
+
+// У змінній topRatedBooks утворився масив книг,
+// рейтинг яких (властивість rating) більший за або дорівнює значенню змінної MIN_RATING.
+
+// У змінній booksByAuthor утворився масив книг,
+// написаних автором з ім'ям (властивість author), яке збігається зі значенням у змінній AUTHOR.
+
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Tell-Tale Heart',
+    author: 'Edgar Allan Poe',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+];
+
+const MIN_RATING = 8;
+const AUTHOR = 'Bernard Cornwell';
+
+const topRatedBooks = books;
+const booksByAuthor = books;
+
+console.log(topRatedBooks); // 8
+console.log(booksByAuthor); // "Bernard Cornwell"
