f3f91cfad0efc451dd39fb4b87f4c65402a0f01a
Romario Korzun <60797864+mrkorzun@users.noreply.github.com>
2026-04-09 10:54:51 +0400

    Refactor task-6.js to comment out example code for future reference

--- js/PW-01/task-6.js
+++ js/PW-01/task-6.js
@@ -1,28 +1,162 @@
+// ==== TASK 1 ====
+
 // Масив books містить колекцію об'єктів книг,
 // кожен з яких містить властивості title, author, rating.
 // Використовуючи метод map(), зроби так,
 // щоб у змінній titles вийшов масив назв усіх книг (властивість title) з масиву books.
 
-const books = [
-  {
-    title: 'The Last Kingdom',
-    author: 'Bernard Cornwell',
-    rating: 8.38,
-  },
-  {
-    title: 'Beside Still Waters',
-    author: 'Robert Sheckley',
-    rating: 8.51,
-  },
-  {
-    title: 'The Tell-Tale Heart',
-    author: 'Edgar Allan Poe',
-    rating: 7.75,
-  },
-  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
-  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
-];
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Tell-Tale Heart',
+//     author: 'Edgar Allan Poe',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
 
-const titles = books.map(book => book.title);
+// const titles = books.map(book => book.title);
 
-console.log(titles); // ["The Last Kingdom", "Beside Still Waters", "The Tell-Tale Heart", "Redder Than Blood", "Enemy of God"]
+// console.log(titles); // ["The Last Kingdom", "Beside Still Waters", "The Tell-Tale Heart", "Redder Than Blood", "Enemy of God"]
+
+// ==== TASK 2 ====
+// Масив books містить колекцію об'єктів книг,
+// кожен з яких містить властивість genres, значенням якої є масив жанрів.
+// Використовуючи метод flatMap(), зроби так,
+// щоб у змінній genres вийшов масив жанрів усіх книг (властивість genres) з масиву books.
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     genres: ['adventure', 'history'],
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     genres: ['fiction'],
+//   },
+//   {
+//     title: 'Redder Than Blood',
+//     author: 'Tanith Lee',
+//     genres: ['horror', 'mysticism'],
+//   },
+// ];
+
+// const genres = books.flatMap(book => book.genres);
+
+// console.log(genres); // genres - це масив ["adventure", "history", "fiction", "horror", "mysticism"]
+
+// ==== TASK 3 ====
+// Цей масив об'єктів ми будемо передавати в параметр users під час виклику функції із завдання.
+
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ['ipsum', 'lorem'],
+//     gender: 'male',
+//     age: 37,
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+//     gender: 'female',
+//     age: 34,
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+//     gender: 'male',
+//     age: 24,
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ['adipisicing', 'irure', 'velit'],
+//     gender: 'female',
+//     age: 21,
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ['ex', 'culpa', 'nostrud'],
+//     gender: 'male',
+//     age: 27,
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ['non', 'amet', 'ipsum'],
+//     gender: 'male',
+//     age: 38,
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ['lorem', 'veniam', 'culpa'],
+//     gender: 'female',
+//     age: 39,
+//   },
+// ];
+
+// const getUserEmails = users => users.map(user => user.email);
+
+// Доповни стрілочну функцію getUserEmails(users) таким чином,
+// щоб вона повертала масив поштових адрес користувачів (властивість email)
+// з масиву об'єктів в параметрі users.
+
+// console.log(getUserEmails(users)); // ["moorehensley@indexia.com", "sharlenebush@tubesys.com", "rossvazquez@xinware.com", "elmahead@omatom.com", "careybarr@nurali.com", "blackburndotson@furnigeer.com", "shereeanthony@kog.com"]
+
+// ==== TASK 4 ====
+// Доповни код так, щоб у змінній evenNumbers був масив парних чисел із масиву numbers,
+// а у змінній oddNumbers — масив непарних.
+// Обов'язково використовуй метод filter().
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+// console.log(evenNumbers);
+// console.log(oddNumbers);
